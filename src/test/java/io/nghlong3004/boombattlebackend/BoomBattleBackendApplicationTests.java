52e5db7fc2bc051b6c3b7f36dcc728ff5e45c42a
nghlong3004 <nghlong3004@gmail.com>
2025-11-13 15:03:13 +0700

    test: added test

---
+++ src/test/java/io/nghlong3004/boombattlebackend/BoomBattleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nghlong3004.boombattlebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoomBattleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
